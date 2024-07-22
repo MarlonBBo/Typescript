69747c01a0f91cd1c6c5696749ce1b4da2ac85a1
marlon <mrlonbarretoborges@gmail.com>
2024-07-22 10:28:46 -0300

    Aula005

---
+++ src/Aula005/index.ts
@@ -0,0 +1,19 @@
+//Array<T> - T[]
+
+export function mult(...args: Array<number>): number {
+    return args.reduce((ac, valor)=> ac * valor);
+}
+
+export function joinStrings(...args: string[]): string {
+    return args.reduce((ac, valor)=> ac + valor);
+}
+
+export function toUpper(...args: string[]): string[] {
+    return args.map((valor)=> valor.toUpperCase());
+}
+
+const multNum = mult(1, 2, 3);
+const join = joinStrings('a', 'b', 'c');
+const joinTwo = toUpper('a', 'b', 'c');
+
+console.log(multNum, join, joinTwo);
